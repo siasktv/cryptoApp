2cd1882f4897634524510f5f7cd6621953476243
siasktv <biankmbm92@gmail.com>
2023-11-12 17:29:35 -0500

    Landing/background

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,10 +2,17 @@ import NavBar from "../components/landingPage/NavBar"
 import Hero from "../components/landingPage/Hero"
 import MarketTrendCards from "../components/landingPage/MarketTrendCards"
 import Features from "../components/landingPage/Features"
+import CryptoChart from "../components/CryptoChart"
+
+// bg-gradient-to-b from-[#FEE6F7] to-white
 
 export default function LandingPage () {
     return (
-        <div className="w-full px-20 py-8 min-h-screen bg-gradient-to-b from-[#FEE6F7] to-white">
+        <div className="w-full px-20 py-8 min-h-screen">
+             <div className="blob w-[800px] h-[800px] rounded-[999px] absolute top-0 right-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200"></div>
+             <div className="blob w-[1000px] h-[1000px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-red-200 via-gray-100 to-blue-100"></div>
+             <div className="blob w-[600px] h-[600px] rounded-[999px] absolute bottom-0 left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-slate-100 via-teal-100 to-blue-100"></div>
+             <div className="blob w-[300px] h-[300px] rounded-[999px] absolute bottom-[-10px] left-0 -z-10 blur-3xl bg-opacity-60 bg-gradient-to-r from-green-200 via-cyan-200 to-Fuchsia-300"></div>
             <NavBar />
             <Hero />
             <div className="">
@@ -31,7 +38,9 @@ export default function LandingPage () {
                 <Features />
             </div>
             </div>
-            
+            <div className="mt-56">
+            <CryptoChart/>
+            </div>
             {/* navbar */}
             {/* hero */}
             {/* marketTrend */}
